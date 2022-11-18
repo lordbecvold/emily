b697d784e898602e22d7049abcecf358eff554e1
lordbecvold <lordbecvold@gmail.com>
2022-11-18 13:16:10 +0100

    Added input validat test

--- src/test/java/ValidatorTest.java
+++ src/test/java/ValidatorTest.java
@@ -1,5 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
+import xyz.becvold.emily.EmilyCore;
 import xyz.becvold.emily.utils.StringUtils;
 
 /**
@@ -8,9 +9,17 @@ import xyz.becvold.emily.utils.StringUtils;
  */
 public class ValidatorTest {
 
+    public EmilyCore core = new EmilyCore();
+
     @Test
     public void unaccentTest() {
         String unaccent = StringUtils.unaccent("ěščřžýáíé");
         Assert.assertEquals("escrzyaie", unaccent);
+    }
+
+    @Test
+    public void inputValidateTest() {
+        String input = core.validateInput("ŠĚŘÍK");
+        Assert.assertEquals("serik", input);
     }
 }
