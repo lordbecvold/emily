06d299f4bb2b1ab4efc6dd74e7986d73e4f0c412
lordbecvold <lordbecvold@gmail.com>
2022-11-21 15:14:36 +0100

    added compatibility warning

--- src/main/java/xyz/becvold/emily/functions/messages/WelcomeMessage.java
+++ src/main/java/xyz/becvold/emily/functions/messages/WelcomeMessage.java
@@ -2,6 +2,7 @@ package xyz.becvold.emily.functions.messages;
 
 import xyz.becvold.emily.utils.ConsoleUtils;
 import xyz.becvold.emily.utils.IntUtils;
+import xyz.becvold.emily.utils.SystemUtils;
 
 /**
  * @author Lukáš Bečvář on 21.11.22
@@ -12,6 +13,7 @@ public class WelcomeMessage {
     // init instances
     public ConsoleUtils console = new ConsoleUtils();
     public IntUtils intUtils = new IntUtils();
+    public SystemUtils systemUtils = new SystemUtils();
 
     // print welcome message
     public void printWelcomeMessage() {
@@ -27,6 +29,11 @@ public class WelcomeMessage {
             console.emilyLog("Ahoj, já jsem Emily mohu ti pomoci?");
         } else {
             console.emilyLog("Ahoj, jmenuji se emily a všechny mé systémy jsou připraveny!");
+        }
+
+        // check if system is linux
+        if (!systemUtils.getOS().equalsIgnoreCase("Linux")) {
+            console.emilyLog("Můj systém je navržen pro všechny operační systémy, ale maximální kompatibilitu zaručuji na Linuxu. :)");
         }
     }
 }
---
+++ src/main/java/xyz/becvold/emily/functions/reactions/WhoAreYouAsk.java
@@ -0,0 +1,5 @@
+package xyz.becvold.emily.functions.reactions;/**
+  * @author Lukáš Bečvář on 21.11.22
+  * @project Emily
+*/public class WhoAreYouAsk {
+}
